f998090f13e4ddb55003e3cbad33b0f70e8d1d99
Salty-dog.dev <stephane.boulanger.1@hotmail.com>
2022-07-07 23:28:28 -0400

    Hello world

---
+++ app/Http/Controllers/TodoListController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\models\ListItem;
+
+class TodoListController extends Controller
+{
+    //
+    public function saveItem(Request $request) {
+        //info(json_encode($request->all()));
+        
+        $newListItem = new ListItem;
+        $newListItem->name = $request->listItem;
+        $newListItem->is_complete = 0;
+        $newListItem->save();
+
+
+        return view('welcome');
+    }
+
+}
---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoListController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('/saveItemRoute', [TodoListController::class, 'saveItem'])->name('saveItem');
+
+
+/*
+Route::get('/saveItem', function () {
+    return view('welcome');
+})->name('saveItem');
+*/
