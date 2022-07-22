77efa90d8b960da9f020dd9edd6b3d3b61a64ee4
Stephane Boulanger <stephboul22@gmail.com>
2022-07-22 14:59:44 -0400

    footer

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,5 @@
 
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -53,7 +52,13 @@
 
         </div>
         
-      
+    <div> 
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/footer.css') }}" >
+    <ft>
+        Copyright Stephane Boulanger - dev ©
+    </ft>
+    </div>  
+
     </body>
 </html>
 
